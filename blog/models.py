404ab6b98dd429fe1ccbf5625740f9073e020da2
AlexandraH-code <ro24nz8c@students.codeinstitute.net>
2025-04-20 11:24:24 +0200

    Add generic post view

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User  # Import models to connect
+from django.contrib.auth.models import User # Import models to connect
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -9,9 +9,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="blog_posts"
+        User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
@@ -21,20 +19,16 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-created_on"]
-
+    
     def __str__(self):
         return f"{self.title} | written by {self.author}"
-     
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
-        Post, 
-        on_delete=models.CASCADE, 
-        related_name="comments")
+        Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
-        User, 
-        on_delete=models.CASCADE, 
-        related_name="commenter")
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
--- codestar/urls.py
+++ codestar/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog.views import my_blog
+from django.urls import path, include
+#from blog.views import my_blog
+#from blog import views as blog_views - uncomment this line to get the blog-line to work
 
 urlpatterns = [
-    path('blog/', my_blog, name='blog'),
+    #path('blog/', my_blog, name='blog'),
+    #path('blog/', blog_views.my_blog, name="blog"),  # the app urls are loaded as the main urls
+    path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
 ]
